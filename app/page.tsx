8c4f1a6b707d3305316732f75c9b68ac26515ae6
wonderakira <akira.takahashi@daily-tohoku.co.jp>
2026-05-20 17:08:13 +0900

    5章まで完了

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import styles from "./page.module.css";
 import Image from "next/image";
-
+import { getNewsList } from "@/app/_libs/microcms";
+import { TOP_NEWS_LIMIT } from "@/app/_constants";
 import NewsList from "@/app/_components/NewsList";
 import ButtonLink from "@/app/_components/ButtonLink";
-import { News } from "@/app/_libs/microcms";
+// import { News } from "@/app/_libs/microcms";
 
 // type News = {
 //   id: string;
@@ -15,43 +16,48 @@ import { News } from "@/app/_libs/microcms";
 //   createdAt: string;
 // };
 
-const data: { contents: News[] } = {
-  contents: [
-    {
-      id: "1",
-      title: "渋谷にオフィスを移転しました",
-      category: {
-        name: "更新情報",
-      },
-      publishedAt: "2023/05/19",
-      createdAt: "2023/05/19",
-    },
-    {
-      id: "2",
-      title: "当社CEOが業界リーダーTOP30に選出されました",
-      category: {
-        name: "更新情報",
-      },
-      publishedAt: "2023/05/19",
-      createdAt: "2023/05/19",
-    },
-    {
-      id: "3",
-      title: "テストの記事です",
-      category: {
-        name: "更新情報",
-      },
-      publishedAt: "2023/05/19",
-      createdAt: "2023/05/19",
-    },
-  ],
-};
+// const data: { contents: News[] } = {
+//   contents: [
+//     {
+//       id: "1",
+//       title: "渋谷にオフィスを移転しました",
+//       category: {
+//         name: "更新情報",
+//       },
+//       publishedAt: "2023/05/19",
+//       createdAt: "2023/05/19",
+//     },
+//     {
+//       id: "2",
+//       title: "当社CEOが業界リーダーTOP30に選出されました",
+//       category: {
+//         name: "更新情報",
+//       },
+//       publishedAt: "2023/05/19",
+//       createdAt: "2023/05/19",
+//     },
+//     {
+//       id: "3",
+//       title: "テストの記事です",
+//       category: {
+//         name: "更新情報",
+//       },
+//       publishedAt: "2023/05/19",
+//       createdAt: "2023/05/19",
+//     },
+//   ],
+// };
 
-const sliceData = data.contents.slice(0, 2);
+// const sliceData = data.contents.slice(0, 2);
 // const sliceData: News[] = [];
 
-export default function Home() {
+export default async function Home() {
   const name = "未来";
+
+  const data = await getNewsList({
+    limit: TOP_NEWS_LIMIT,
+  });
+
   return (
     <>
     <section className={styles.top}>
@@ -63,7 +69,7 @@ export default function Home() {
     </section>
     <section className={styles.news}>
       <h2 className={styles.newsTitle}>News</h2>
-      <NewsList news={sliceData} />
+      <NewsList news={data.contents} />
       {/* <ul>
         {sliceData.map((article) => (
           <li key={article.id} className={styles.list}>
